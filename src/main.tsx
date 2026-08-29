d071e0d4747b8b2e2f7d004973a8f2c6bb6b18de
ajeuka25 <ajeuka2503@gmail.com>
2026-08-29 16:55:54 +0700

    Updated index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Web3AuthProvider } from '@web3auth/modal/react';
 import App from './App.tsx';
+import { web3AuthContextConfig } from './web3authContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Web3AuthProvider config={web3AuthContextConfig}>
+      <App />
+    </Web3AuthProvider>
   </StrictMode>
 );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+readonly VITE_PINATA_JWT?: string;
+readonly VITE_WEB3AUTH_CLIENT_ID?: string;
