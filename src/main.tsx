833d55e4d8a171247cc40365c2f7e2e34966e763
Gefallen <zainpraya.mj.1@gmail.com>
2026-08-30 16:49:17 +0700

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Web3AuthProvider } from '@web3auth/modal/react';
 import App from './App.tsx';
-import { web3AuthContextConfig } from './web3authContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Web3AuthProvider config={web3AuthContextConfig}>
-      <App />
-    </Web3AuthProvider>
+    <App />
   </StrictMode>
 );
